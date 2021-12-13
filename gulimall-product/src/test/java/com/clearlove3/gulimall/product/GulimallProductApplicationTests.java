68a16465a17e189852d020e551af92f67210f1cc
clearlove3 <clearlovefan@gmail.com>
2021-12-13 15:59:51 +0800

    完成p65部分，由于idea卡顿暂停工作，前端部分稍微看了下，主要专心于后台部分。

--- gulimall-product/src/test/java/com/clearlove3/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/clearlove3/gulimall/product/GulimallProductApplicationTests.java
@@ -1,22 +1,52 @@
 package com.clearlove3.gulimall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSClientBuilder;
 import com.clearlove3.gulimall.product.entity.BrandEntity;
 import com.clearlove3.gulimall.product.service.BrandService;
+import org.checkerframework.checker.guieffect.qual.AlwaysSafe;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
     @Autowired
     BrandService brandService;
+    @Autowired
+    OSSClient ossClient;
+
     @Test
     void contextLoads() {
-        BrandEntity brandEntity= new BrandEntity();
+        BrandEntity brandEntity = new BrandEntity();
         brandEntity.setName("苹果");
         brandService.save(brandEntity);
         System.out.println("保存成功");
     }
 
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        /*// yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+        String endpoint = "oss-cn-guangzhou.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5tDfBPjYJskXYGK1aTBi";
+        String accessKeySecret = "UxFhHD7xt0FpTwEYIPfz4XpcskvqXM";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+*/
+// 填写本地文件的完整路径。如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+        InputStream inputStream = new FileInputStream("D:\\壁纸\\pixiv\\pixiv_55.jpg");
+// 依次填写Bucket名称（例如examplebucket）和Object完整路径（例如exampledir/exampleobject.txt）。Object完整路径中不能包含Bucket名称。
+        ossClient.putObject("gulimall-clearlove3", "2.jpg", inputStream);
+        System.out.println("上传完成");
+// 关闭OSSClient。
+        ossClient.shutdown();
+    }
 }
